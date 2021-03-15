a2c6cab7f2d84e31d993131049073751c2095ba0
nalendro16 <nalendro_agung@yahoo.com>
2021-03-15 23:23:38 +0700

    use effect fetch and error handling

--- src/Home.js
+++ src/Home.js
@@ -4,26 +4,37 @@ import Bloglist from './Bloglist'
 const Home = () => {
   // let name = 'Marco'
   const [blogs, setBlogs] = useState(null)
+  const [pending, setPending] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((res) => {
-        return res.json()
-      })
-      .then((data) => {
-        console.log(data)
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:3000/blogss')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('Failed fetch from the resources!')
+          }
+          return res.json()
+        })
+        .then((data) => {
+          console.log(data)
+          setBlogs(data)
+          setPending(false)
+          setError(null)
+        })
+        .catch((err) => {
+          setPending(false)
+          setError(err.message)
+          setBlogs(null)
+        })
+    }, 100)
   }, [])
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {pending && <div>Load...</div>}
       {blogs && <Bloglist blogs={blogs} />}
-      {/* <Bloglist
-        blogs={blogs.filter((blog) => blog.author == 'pinkman')}
-        title="Mario's blog"
-      /> */}
-      {/* make useable component */}
     </div>
   )
 }
