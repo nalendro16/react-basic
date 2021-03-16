93f9a9204d0407b6da72e6c3d9c41a790f16d26e
nalendro16 <nalendro_agung@yahoo.com>
2021-03-16 23:40:15 +0700

    controller input and 2way data binds

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,39 @@
+import { useState } from 'react'
+
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('walter')
+
   return (
-    <div>
-      <h2>This is add blog</h2>
+    <div className="create">
+      <h2>Write a new blog</h2>
+      <form>
+        <label className="title-create">Blog Title: {title}</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body: {body}</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author: {author}</label>
+        <select
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="walter">walter</option>
+          <option value="pinkman">pinkman</option>
+          <option value="walter">tuco</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   )
 }
