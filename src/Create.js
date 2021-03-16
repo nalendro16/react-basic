66ff21e7804c59cd8c7b1cbe86e23fb8b8734921
nalendro16 <nalendro_agung@yahoo.com>
2021-03-17 00:17:36 +0700

    redirect after POST success

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { useHistory } from 'react-router'
 
 const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('walter')
   const [isPending, setisPending] = useState(false)
+  const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -19,6 +21,7 @@ const Create = () => {
     }).then(() => {
       console.log('new blog added')
       setisPending(false)
+      history.push('/')
     })
   }
 
